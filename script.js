315d2989b1f9463f1f66c9616d6c3bf7c93661e6
sreekanth25041990 <71996584+sreekanth25041990@users.noreply.github.com>
2022-01-27 13:03:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function calculate() {
+
+    const newDate = new Date();
+    newDate.setFullYear(newDate.getFullYear() );
+
+
+
+    p=document.getElementById("p").value;
+    n=document.getElementById("n").value;
+    r=document.getElementById("r").value;
+    si=(p*n*r/100);
+    sum= (parseInt(si)+parseInt(p));
+    result=document.getElementById("result");
+    result.innerHTML="If you deposit " + p + "," + "<br>"+ "at an interest rate of " + n + "%" + "." + "<br>"+ "you will receive an amount of " + sum + "," +"<br>" + "in the year " +  (newDate.getFullYear()+parseInt(r));
+
 }
-        
