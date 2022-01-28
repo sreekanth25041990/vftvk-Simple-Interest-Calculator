27f578d52734cd06ec6e735b33526ab1cf4ca510
sreekanth25041990 <71996584+sreekanth25041990@users.noreply.github.com>
2022-01-28 14:52:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,4 +13,20 @@ function calculate() {
     result=document.getElementById("result");
     result.innerHTML="If you deposit " + p + "," + "<br>"+ "at an interest rate of " + n + "%" + "." + "<br>"+ "you will receive an amount of " + sum + "," +"<br>" + "in the year " +  (newDate.getFullYear()+parseInt(r));
 
+    if (p==""){
+        alert("please enter a positive number")
+        return false
+    }
+
+    if(p== 0){ 
+        alert("please enter a positive number")
+    
+    }
+
+    if (p<0){
+        alert("please enter a positive number")
+    }
+
+
+    
 }
